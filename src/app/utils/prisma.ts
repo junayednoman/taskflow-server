fdccbec8031518ba2a15e34cc2c22335252bac34
junayednoman <junayednoman05@gmail.com>
2025-11-20 20:41:12 +0600

    feat: db url added to db block in prisma config

--- src/app/utils/prisma.ts
+++ src/app/utils/prisma.ts
@@ -1,5 +1,12 @@
 import { PrismaClient } from "@prisma/client";
+import config from "../config";
 
-const prisma = new PrismaClient({});
+const prisma = new PrismaClient({
+  datasources: {
+    db: {
+      url: config.db_url,
+    },
+  },
+});
 
 export default prisma;
